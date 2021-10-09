8488313b4cf9e80b771a8049ab943a9d67dd4da2
this-is-bear <mikhailaushev2007@gmail.com>
2021-10-09 18:16:08 +0300

    Add task2 task

--- task2.py
+++ task2.py
@@ -1,3 +1,13 @@
 class TriangleChecker:
-    
-    def __init__(self, add='')
+    def __init__(self, sides):
+        self.sides = sides
+
+    def is_triangle(self):
+        if all(isinstance(side, (int, float)) for side in self.sides):
+            if all(side > 0 for side in self.sides):
+                sorted_sides = sorted(self.sides)
+                if sorted_sides[0] + sorted_sides[1] > sorted_sides[2]:
+                    return 'Ура, можно построить треугольник!'
+                return 'Жаль, но из этого треугольник не сделать'
+            return 'С отрицательными числами ничего не выйдет!'
+        return 'Нужно вводить только числа!'
